e31f3fee0e2101c6e383530181e5bb5f0e62fabb
sergeev1706 <140341803+sergeev1706@users.noreply.github.com>
2023-08-17 10:02:30 +0300

    add files

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,5 @@
 
 
-
 let enterBtn = document.querySelector('#enterBtn');
 let inp = document.querySelector('input');
 let adminBtn = document.querySelector('#admin-btn');
@@ -9,7 +8,7 @@ let textarea = document.querySelector('#textarea');
 enterBtn.addEventListener('click', (e) => {
   e.preventDefault();
 
-  if (inp.value === 'admin') {
+  if (inp.value === 'ne_admin') {
     inp.value = '';
     window.location.href = '/admin';
   } else {
@@ -23,25 +22,13 @@ inp.onfocus = () => {
   inp.value = '';
 };
 
-if (adminBtn) {
-  adminBtn.addEventListener('click', () => {
-    // textarea.classList.remove('disable');
-    // adminBtn.classList.add('disable');
-
-    // обработать запрос post 
-    // для добавления анекдота
-
-  })
-}
-
 // ---------------- pagination -------------
 
-let pages = document.querySelectorAll('.pagination')
+let pages = document.querySelectorAll('.pagination');
 
 for (const page of pages) {
   page.addEventListener('click', () => {
-    // console.log(page.textContent);
-    window.location.href = `/list/page/${page.textContent}`
+    window.location.href = `/list/page/${page.textContent}`;
   })
 }
 
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 
 import express from 'express';
 import expressHandlebars from 'express-handlebars';
+
 import { jokes } from './content/jokes.js';
 import bodyParser from 'body-parser';
+
+import flash from 'connect-flash';
+import cookieParser from 'cookie-parser';
+import expressSession from 'express-session';
 
 const handlebars =
   expressHandlebars.create({
@@ -35,47 +40,168 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(express.static(__dirname + '/public/'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let secret = 'qwerty';
+app.use(cookieParser(secret));
+app.use(expressSession({
+
+  secret: secret,
+}));
+
+app.use(flash());
+
+function getUnicueList() {
+
+  let topics = [];
+  jokes.map(el => topics.push(el.topic));
+  let unicuTopics = [... new Set(topics)];
+
+  return unicuTopics;
+}
+
+function getMassage(param) {
+  let wrapper = param;
+
+  let massage = '';
+  if (wrapper[0] !== undefined) {
+    massage = wrapper[0];
+  }
+  wrapper = [];
+  return massage;
+}
+
 // --------------- pagination -------------
 
 let page = 1; // номер выводимой страницы
 let countElem = 3; // количество выводимых анекдотов на странице
-let countPages = Math.ceil(jokes.length / countElem); // количество страниц
-
-let pages = []
-for (let i = 0; i < countPages; i++) {
-  pages.push(i + 1)
-}
+let countPages; // количество страниц
 
 // ----------------------------------------
 
-app.get('/', (req, res) => {
-  res.render('main', { text: 'aaa', title: 'анекдоты главная' });
-});
-
-app.get('/admin', (req, res) => {
-  res.render('admin', { title: 'страница администратора' });
-});
-
-app.post('/admin', (req, res) => {
-  console.log(req.body);
-  res.render('admin', { title: 'страница администратора' });
-})
-
+// главная список с пагинацией
 app.get('/list/page/:num', (req, res) => {
   page = req.params.num;
 
   let start = (page - 1) * countElem;
-  let end = page * countElem
+  let end = page * countElem;
   let outputRecords = jokes.slice(start, end);
+
+  countPages = Math.ceil(jokes.length / countElem);
+
+  let pages = [];;
+  for (let i = 0; i < countPages; i++) {
+    pages.push(i + 1);
+  }
 
   res.render('list', {
     title: 'список анекдотов',
     jokes: outputRecords,
-    pages: pages
+    pages: pages,
+    topics: getUnicueList(),
   })
 })
 
-app.use(function (req, res) {
+app.get('/list/:cat', (req, res) => {
+  let category = req.params.cat;
+
+  let result = [];
+
+  jokes.map(el => {
+    if (el.topic === category) result.push(el);
+  })
+
+  res.render('list', {
+    title: `список анекдотов категории ${category}`,
+    jokes: result,
+    topics: getUnicueList(),
+  })
+})
+
+app.get('/admin', (req, res) => {
+
+  res.render('admin', {
+    title: 'страница администратора',
+    jokes: jokes,
+    topics: getUnicueList(),
+  });
+});
+
+// добавление анекдота
+app.post('/admin', (req, res) => {
+
+  req.flash('joke', 'вы добавили анекдот');
+
+  let topic;
+  if (req.body.newCat) {
+    topic = req.body.newCat;
+  } else {
+    topic = req.body.topic;
+  }
+
+  jokes.push({
+    id: jokes.length + 1,
+    text: req.body.text,
+    topic: topic,
+  });
+
+  res.render('admin', {
+    title: 'страница администратора',
+    jokes: jokes,
+    topics: getUnicueList(),
+    massage: getMassage(req.flash('joke')),
+  });
+})
+
+// удаление анекдота
+app.get('/delete/:id', (req, res) => {
+  jokes.map((el, ind) => {
+    if (el.id == req.params.id) jokes.splice(ind, 1);
+  })
+
+  req.flash('joke', 'вы удалили анекдот');
+
+  res.render('admin', {
+    title: 'страница администратора',
+    jokes: jokes,
+    topics: getUnicueList(),
+    massage: getMassage(req.flash('joke')),
+  });
+})
+
+// изменение анекдота
+app.get('/update/:id', (req, res) => {
+
+  let joke;
+  jokes.map(el => {
+    if (el.id == req.params.id) {
+      joke = el;
+    }
+  })
+
+  res.render('update', {
+    joke: joke,
+  })
+})
+
+app.post('/update/:id', (req, res) => {
+
+  jokes.map(el => {
+    if (el.id == req.params.id) {
+      el.topic = req.body.topic;
+      el.text = req.body.text;
+    }
+  })
+
+  req.flash('joke', 'вы изменили анекдот')
+
+  res.render('admin', {
+    title: 'страница администратора',
+    jokes: jokes,
+    topics: getUnicueList(),
+    massage: getMassage(req.flash('joke')),
+  });
+})
+
+app.use((req, res) => {
   res.status(404).send('not found');
 });
 
